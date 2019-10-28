8c7a2c96def14ef16fad7a38ef956bd720e54916
uncultivatedrabbit <uncultivatedrabbit@gmail.com>
2019-10-27 20:39:45 -0400

    updated functionality

--- src/components/layout/Selector.js
+++ src/components/layout/Selector.js
@@ -40,21 +40,31 @@ class Selector extends Component {
             <div className="d-flex align-items-center justify-content-center">
               {this.props.prevPokemonId > 0 ?
                 <React.Fragment>
-                  <img className="pb-5 mx-2" alt={`${this.props.prevPokemonName}`} src={
-                    this.state.currentPokemonId < 650 ? 
+                  <img className="pb-5 mx-2" 
+                      alt={`${this.props.prevPokemonName}`} 
+                      src={
+                        this.state.currentPokemonId < 650 ? 
 
-                    `https://img.pokemondb.net/sprites/black-white/anim/normal/${this.props.prevPokemonName}.gif`
-                  : ""
+                      `https://img.pokemondb.net/sprites/black-white/anim/normal/${this.props.prevPokemonName}.gif`
+                      : ""
 
-                  }/>
-                  <Anchor href={`/pokemon/pokemongen/pokemon/${this.props.prevPokemonId}/`}> {this.props.prevPokemonName.charAt(0).toUpperCase() + this.props.prevPokemonName.slice(1)}</Anchor>
+                    }/>
+                  <Anchor 
+                    href={`/pokemon/pokemongen/pokemon/${this.props.prevPokemonId}/`}> 
+                    {this.props.prevPokemonName.charAt(0).toUpperCase() + this.props.prevPokemonName.slice(1)}
+                  </Anchor>
                 </React.Fragment>
               : null}
             </div>
             {this.props.nextPokemonId !== "" ? 
               <div className="d-flex align-items-center justify-content-center">
-              <img className="pb-5 mx-2" alt={`${this.props.nextPokemonName}`} src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${this.props.nextPokemonName}.gif`}/>
-              <Anchor href={`/pokemon/pokemongen/pokemon/${this.props.nextPokemonId}/`}> {this.props.nextPokemonName.charAt(0).toUpperCase() + this.props.nextPokemonName.slice(1)}</Anchor>
+              <img className="pb-5 mx-2" 
+                  alt={`${this.props.nextPokemonName}`} 
+                  src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${this.props.nextPokemonName}.gif`}/>
+              <Anchor 
+                href={`/pokemon/pokemongen/pokemon/${this.props.nextPokemonId}/`}> 
+                {this.props.nextPokemonName.charAt(0).toUpperCase() + this.props.nextPokemonName.slice(1)}
+              </Anchor>
               </div>
               :
               ""}
